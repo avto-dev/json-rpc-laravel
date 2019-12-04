02167ea7c37479baab008805bc490b3b359ef002
eldario <eldario666@gmail.com>
2019-12-04 15:29:18 +0500

    Added Incomplete tests

---
+++ tests/ServiceProviderBoundIfTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AvtoDev\JsonRpc\Tests;
+
+/**
+ * @covers \AvtoDev\JsonRpc\ServiceProvider<extended>
+ */
+class ServiceProviderBoundIfTest extends AbstractTestCase
+{
+    protected $register_service_provider = false;
+
+    /**
+     * @return void
+     */
+    public function testRegisterJsonRpcRequestsFactory(): void
+    {
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * @return void
+     */
+    public function testRegisterRpcKernel(): void
+    {
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * @return void
+     */
+    public function testRegisterRpcRouter(): void
+    {
+        $this->markTestIncomplete();
+    }
+}
